2968bc76edb0c80feb05503204863be33d164b12
vmhoang1703 <vmhoang1703@gmail.com>
2023-08-20 20:44:55 +0700

    Last update was level 4 using bcrypt. And this update is level 5 that I Use Passport.js to Add Cookies and Sessions

--- app.js
+++ app.js
@@ -3,18 +3,28 @@ import 'dotenv/config';
 import express from "express";
 import ejs from "ejs";
 import bodyParser from "body-parser";
-import mongoose, { model, mongo } from "mongoose";
-import bcrypt from "bcrypt";
+import mongoose from "mongoose";
+import session from 'express-session';
+import passport from 'passport';
+import passportLocalMongoose from "passport-local-mongoose";
 
 const app = express();
 const port = 3000;
-const saltRounds = 10;
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect("mongodb://127.0.0.1:27017/userDB");
+app.use(session({
+    secret: "Our little secret.",
+    resave: false,
+    saveUninitialized: false,
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect("mongodb://127.0.0.1:27017/userDB", { useNewUrlParser: true });
 
 const userSchema = new mongoose.Schema({
     email: String,
@@ -22,66 +32,69 @@ const userSchema = new mongoose.Schema({
     secret: String
 });
 
+userSchema.plugin(passportLocalMongoose);
+
 const User = mongoose.model("User", userSchema);
 
+passport.use(User.createStrategy());
 
-app.get("/", async(req, res) => {
-    try {
-        res.render("home.ejs");
-    } catch (error) {
-        res.status(500).send(error);
-    }
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.get("/", (req, res) => {
+    res.render("home.ejs");
 });
 
-app.get("/login", async(req, res) => {
-    try {
-        res.render("login.ejs");
-    } catch (error) {
-        res.status(500).send(error);
-    }
+app.get("/login", (req, res) => {
+    res.render("login.ejs");
 });
 
-app.get("/register", async(req, res) => {
-    try {
-        res.render("register.ejs");
-    } catch (error) {
-        res.status(500).send(error);
-    }
+app.get("/register", (req, res) => {
+    res.render("register.ejs");
 });
 
-app.post("/register", async(req, res) => {
-    const email = req.body.username;
-    const password = req.body.password;
-    try {
-        const hashedPassword = bcrypt.hashSync(password, saltRounds);
-        const hashedSecret = bcrypt.hashSync(process.env.SECRET, saltRounds);
-        const newUser = new User({
-            email: email,
-            password: hashedPassword,
-            secret: hashedSecret
-        });
-        await newUser.save();
+app.get("/secrets", (req, res) => {
+    if (req.isAuthenticated()) {
         res.render("secrets.ejs");
-    } catch (error) {
-        res.status(500).send(error);
+    } else {
+        res.redirect("/login");
     }
 });
 
-app.post("/login", async(req, res) => {
-    const email = req.body.username;
-    const password = req.body.password;
-    try {
-        const userFound = await User.findOne({ email: email });
-        const resultCompare = bcrypt.compareSync(password, userFound.password);
-        if( resultCompare ) {
-            res.render("secrets.ejs");
+app.get("/logout", (req, res) => {
+    req.logout(() => {
+        res.redirect("/");
+    })
+});
+
+app.post("/register", (req, res) => {
+    User.register({username: req.body.username}, req.body.password, (err, user) => {
+        if (err) {
+            console.log(err);
+            res.redirect("/register");
         } else {
-            res.redirect("/login");
+            passport.authenticate("local")(req, res, () => {
+                res.redirect("/secrets");
+            });
         }
-    } catch (error) {
-        res.status(500).send(error);
-    }
-})
+    });
+});
+
+app.post("/login", (req, res) => {
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+    req.login(user, (err) => {
+        if (err) {
+            console.log(err);
+        } else {
+            passport.authenticate("local")(req, res, () => {
+                res.redirect("/secrets");
+            });
+        }
+    });
+});
 
 app.listen(port, () => {
     console.log(`Server has started on port ${port}.`);
