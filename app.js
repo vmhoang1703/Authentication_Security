db6fb0159c07c8a23c38368d50cf82aae91b7ec0
vmhoang1703 <vmhoang1703@gmail.com>
2023-08-21 10:44:59 +0700

    Level 6 - OAuth 2.0 & How to Implement Sign In with Google

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-//jshint esversion:6
-import 'dotenv/config';
+import "dotenv/config";
 import express from "express";
 import ejs from "ejs";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
-import session from 'express-session';
-import passport from 'passport';
+import session from "express-session";
+import passport from "passport";
 import passportLocalMongoose from "passport-local-mongoose";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import findOrCreate from "mongoose-findorcreate";
 
 const app = express();
 const port = 3000;
@@ -24,25 +25,61 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-mongoose.connect("mongodb://127.0.0.1:27017/userDB", { useNewUrlParser: true });
+mongoose.connect("mongodb://127.0.0.1:27017/userDB", {
+    useNewUrlParser: true,
+});
 
 const userSchema = new mongoose.Schema({
     email: String,
     password: String,
-    secret: String
+    googleId: String // Add this line to store Google's unique ID
 });
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 const User = mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser((user, cb) => {
+    cb(null, user.id);
+});
+
+passport.deserializeUser((id, cb) => {
+    User.findById(id).exec()
+      .then(user => {
+        cb(null, user);
+      })
+      .catch(err => {
+        cb(err);
+      });
+  });
+  
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+}, (accessToken, refreshToken, profile, cb) => {
+    User.findOrCreate({ googleId: profile.id }, (err, user) => {
+        return cb(err, user);
+    });
+}));
 
 app.get("/", (req, res) => {
     res.render("home.ejs");
+});
+
+app.get("/auth/google",
+  passport.authenticate("google", { scope: ["profile"] }));
+
+app.get("/auth/google/secrets", 
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  (req, res) => {
+    // Successful authentication, redirect to secrets page.
+    res.redirect("/secrets");
 });
 
 app.get("/login", (req, res) => {
@@ -64,11 +101,11 @@ app.get("/secrets", (req, res) => {
 app.get("/logout", (req, res) => {
     req.logout(() => {
         res.redirect("/");
-    })
+    });
 });
 
 app.post("/register", (req, res) => {
-    User.register({username: req.body.username}, req.body.password, (err, user) => {
+    User.register({ username: req.body.username }, req.body.password, (err, user) => {
         if (err) {
             console.log(err);
             res.redirect("/register");
